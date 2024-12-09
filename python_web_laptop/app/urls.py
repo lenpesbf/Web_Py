2b548bd5261ca98a3c325fe0b6bea474683cdd05
BinhTranThanhNLU <22130027@st.hcmuaf.edu.vn>
2024-12-09 22:07:31 +0700

    moi them duong vai lien ket o trang home thoi

--- python_web_laptop/app/urls.py
+++ python_web_laptop/app/urls.py
@@ -3,18 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view),
-    path('login/', views.login_view),
-    path('verify/', views.verify_view),
-    path('error/', views.error_view),
-    path('about-us/', views.about_us_view),
-    path('checkout/', views.checkout_view), 
-    path('compare/', views.compare_view),
-    path('contact/', views.contact_view),
-    path('faq/', views.faq_view), 
-    path('payment/', views.payment_view), 
-    path('shop-left-sidebar/', views.shop_left_sidebar_view), 
-    path('cart/', views.cart_view),
-    path('detail/', views.single_product_view),
-    path('wishlist/', views.wishlist_view)
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('verify/', views.verify_view, name='verify'),
+    path('error/', views.error_view, name='error'),
+    path('about-us/', views.about_us_view, name='about-us'),
+    path('checkout/', views.checkout_view, name='checkout'), 
+    path('compare/', views.compare_view, name='compare'),
+    path('contact/', views.contact_view, name='contact'),
+    path('faq/', views.faq_view, name='faq'), 
+    path('payment/', views.payment_view, name='payment'), 
+    path('shop-left-sidebar/', views.shop_left_sidebar_view, name='shop-left-sidebar'), 
+    path('cart/', views.cart_view, name='cart'),
+    path('detail/', views.single_product_view, name='detail'),
+    path('wishlist/', views.wishlist_view, name='wishlist')
 ]
